57651f1951c596aa6010af4a4dc70d17b58fe938
Varsha Chary <34280450+varshachary@users.noreply.github.com>
2021-04-30 14:13:36 +0530

    Add files via upload

---
+++ dumcharades.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 30 14:09:25 2021
+
+@author: varsha
+"""
+
+import random
+import list1
+random.choice(list1) 
---
+++ movie_name.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 30 14:08:36 2021
+
+@author: varsha
+"""
+
+           
+list1=['Kadhalai Thavire Verondrum Illai',
+'Tharkappu',
+'Aindhaam Thalaimurai Sidha Vaidhiya Sigamani',
+'Meenakshi Kadhalan Ilangovan',
+'Thigilodu Vilaiyaadu',
+'Mudhal Thagaval Arikkai',
+'Yokkiyan Varan Somba Thooki Ulla Vai',
+'Kanden Kadhal Konden',
+'Kallathoni',
+'Kuttrame Thandanai',
+'Pachaikili Parimala',
+'Amara Kaaviyam',
+'Mupparimanam',
+'Keikraan Meikkiran',
+'Vishayam Veliye Theriya Koodadhu',
+'Purampokku Engira Podhuvudamai',
+'Kamara Kattu',
+'Puriyadha Anandam Puthithaga Arambam',
+'Tamilselvanum 50KM Kalaiselviyum',
+'Murugatrupadai',
+'Ariyathavan Puriyathavan',
+'Nagaraja Cholan MA, MLA',
+'Yagavarayinum Naa Kaakka',
+'Miss Pannidatheenga Appuram Varuthapaduveenga',
+'Dummy Tappasu',
+'Nenju Irukkum Varai Ninaivu Irukkum',
+'Summa Nachunu Irukku',
+'Ingu Kadhal Katrutharapadum',
+'Naveena Saraswathi Sabatham',
+'Sengathu Bhoomiyilae',
+'Aayiram Muthangaludam Thenmozhi',
+'Nil Gavani Sellathey',
+'Injimarappa',
+'Athimalai Muthupandi',
+'Madhavanum Malarvizhiyum',
+'Rettai Kadhir',
+'Kerala Nattilam Pengaludane',
+'Karpavai Katrapin',
+'Sabash Sariyaana Potti',
+'Pongadi Neengalum Unga Kaadhalum',
+]  
